ef5883b84a548cdf2edc4088def0be85376b0ae9
heejin1023 <heejin1023dev@gmail.com>
2024-08-02 11:37:10 +0900

    myselectshop - scheduler

--- src/test/java/com/sparta/myselectshop/MyselectshopApplicationTests.java
+++ src/test/java/com/sparta/myselectshop/MyselectshopApplicationTests.java
@@ -8,6 +8,23 @@ class MyselectshopApplicationTests {
 
     @Test
     void contextLoads() {
+        int x = 4;
+        int n = 3;
+        long[] answer = new long[n];
+
+        for(int i = 0; i < answer.length; i++) {
+            if(i == 0) {
+                answer[i] = x;
+                continue;
+            }
+
+            answer[i] = answer[i - 1] + x;
+            System.out.println(i + " " + answer[i]);
+        }
+        System.out.println("----");
+        for(int i = 0; i < answer.length; i++) {
+            System.out.println(answer[i]);
+        }
     }
 
 }
